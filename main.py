8042ba22ab55b160add1cd872cd8a4e59831e969
mundo20guz <mundo_guzman@yahoo.com>
2019-05-10 14:38:01 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,10 @@
-# Super Tic-Tac-Toe
-# author: Andrew Nguyen
-#
-from turtle import *
-import random
-def main():
-    print("Main loop start")
-board1 = [['','',''],['','',''],['','','']]
-board2 = [['','',''],['','',''],['','','']]
-board3 = [['','',''],['','',''],['','','']]
-board4 = [['','',''],['','',''],['','','']]
-board5 = [['','',''],['','',''],['','','']]
-board6 = [['','',''],['','',''],['','','']]
-board7 = [['','',''],['','',''],['','','']]
-board8 = [['','',''],['','',''],['','','']]
-board9 = [['','',''],['','',''],['','','']]
-turtle = Turtle()
+from board import *
+from player import *
+from game import *
+from minimax import *
 
-def drawBoard():
-    turtle.clear()
-    turtle.color("black")
-    turtle.pensize(5)
-
-    #horizontal
-    for i in range(2):
-        turtle.pu()
-        turtle.goto(-150,50-100*i)
-        turtle.setheading(0)
-        turtle.pd()
-        turtle.forward(300)
-
-    #vertical
-    for i in range(2):
-        turtle.pu()
-        turtle.goto(-50+100*i,150)
-        turtle.setheading(270)
-        turtle.pd()
-        turtle.forward(300)
-# TODO
-# def makeMove():
-#
-#
-
-if __name__ == "main":
-    main()
+S = Super_Board()
+MiniMax = AlphaBetaAgent(GridState.PLAYER_X)
+HumanPlayer = HumanPlayer(GridState.PLAYER_O)
+game = Game(S,MiniMax,HumanPlayer)
+game.startGame()
