c7e58971ceaf8f9af8f92e0815e2ff8db3a5ea2b
mundo20guz <mundo_guzman@yahoo.com>
2019-05-10 14:34:30 -0700

    Update player.py

--- player.py
+++ player.py
@@ -11,14 +11,12 @@
 # Includes code for Player. Creates both User and AI players.
 #
 # Dependencies
-import sys
-import os
+import random
 from board import *
-from enum import Enum
 
 ######################################################################
 
-class AI_difficulty(Enum):
+class AI_difficulty():
 	""" Chooses AI difficulty """
 	EASY = None
 	MED = None
@@ -26,26 +24,49 @@ class AI_difficulty(Enum):
 
 ###################### Normal Player #################################
 
-class Player():
+class RandomPlayer():
 
-	def __init__(self):
-		self.board = None
-		self.player = None
+	def __init__(self,playerName):
+		self.playerName = playerName
 
-	def makeMove(self,i_pos,j_pos):
-		""" Creates Players next move """
-		pass
 
+	def makeMove(self,board,game=None):
+		# Next Grid is unspecified (i.e. first move or playing after grid was won)
+		if board.nextGrid == [None,None]:
+			# Choose at random to play another open grid
+			openBoards = board.availableBoards()
+			if openBoards:
+				nextGrid = random.choice(openBoards)
+				nextMove = random.choice(board.emptySpaces(nextGrid))
+				board.makeMove(self.playerName,nextGrid[0],nextGrid[1],nextMove[0],nextMove[1])
+		else:
+			nextMove = random.choice(board.emptySpaces(board.nextGrid))
+			board.makeMove(self.playerName,board.nextGrid[0],board.nextGrid[1],nextMove[0],nextMove[1])
 
 ######################## AI Opponent ##################################
+class HumanPlayer():
 
-class AI_Player(Player):
+	def __init__(self,playerName):
+		self.playerName = playerName
 
-	def __init__(self):
-		""" Calls super constructor of Player Class """
-		super.__init__(self,board,player)
 
-	def makeMove(self,i_pos,j_pos):
-		""" Creates AIs next move """
-		pass
+	def makeMove(self,board,game=None):
+		# Next Grid is unspecified (i.e. first move or playing after grid was won)
+		if board.nextGrid == [None,None]:
+			# Choose at random to play another open grid
+			openBoards = board.availableBoards()
+			if openBoards:
+				board.print_board()
+				print(openBoards)
+				nextGrid = input('Choose which grid to play on..').split()
+				board.board[int(nextGrid[0])][int(nextGrid[1])].print_board()
+				nextMove = input('Where would you like to go?').split()
+				board.makeMove(self.playerName,int(nextGrid[0]),int(nextGrid[1]),int(nextMove[0]),int(nextMove[1]))
+		else:
+			board.print_board()
+			print('Your move will be on grid {}'.format(board.nextGrid))
+			board.board[board.nextGrid[0]][board.nextGrid[1]].print_board()
+			nextMove = input('Where would you like to go?').split()
+			#nextMove = random.choice(board.emptySpaces(board.nextGrid))
+			board.makeMove(self.playerName,board.nextGrid[0],board.nextGrid[1],int(nextMove[0]),int(nextMove[1]))
 
